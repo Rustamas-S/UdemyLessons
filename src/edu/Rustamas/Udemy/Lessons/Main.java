981a9daa858fae8f7cd765a7c5414afe1cd6579f
Rustamas <rustams@live.com>
2020-10-04 14:02:19 +0300

    2020-10-04

--- src/edu/Rustamas/Udemy/Lessons/Main.java
+++ src/edu/Rustamas/Udemy/Lessons/Main.java
@@ -224,20 +224,20 @@ public class Main {
         myVariable--;
         System.out.println("This is another one");
 
-//Lesson 45
+        //Lesson 45
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if(score < 5000 && score >1000) {
-//            System.out.println("This was was less than 5000 but greater than 1000");
-//        } else if(score < 1000) {
-//            System.out.println("Your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//
-//        }
+        //        if(score < 5000 && score >1000) {
+        //            System.out.println("This was was less than 5000 but greater than 1000");
+        //        } else if(score < 1000) {
+        //            System.out.println("Your score was less than 1000");
+        //        } else {
+        //            System.out.println("Got here");
+        //
+        //        }
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
@@ -251,15 +251,15 @@ public class Main {
         // bonus set to 200
         // But make sure the first printout above still displays as well
 
-//        boolean newGameOver = true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-//
-//        if(newGameOver) {
-//            int finalScore = newScore + (newLevelCompleted * newBonus);
-//            System.out.println("Your final score was " + finalScore);
-//        }
+        //        boolean newGameOver = true;
+        //        int newScore = 10000;
+        //        int newLevelCompleted = 8;
+        //        int newBonus = 200;
+        //
+        //        if(newGameOver) {
+        //            int finalScore = newScore + (newLevelCompleted * newBonus);
+        //            System.out.println("Your final score was " + finalScore);
+        //        }
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
